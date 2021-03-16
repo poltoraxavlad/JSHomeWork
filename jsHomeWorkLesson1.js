b0d3f68a83a5274290833e5fee186d22e271fd18
Vladyslav.Poltoratsk <poltoraxavlad@gmail.com>
2021-03-16 20:19:28 +0200

    Lesson 1 and 2

---
+++ jsHomeWorkLesson1.js
@@ -0,0 +1,6 @@
+var DrawCats = function (howManyTimes) {
+    for (var i = 0; i < howManyTimes; i++) {
+        console.log(i + "=^.^=");
+    }
+};
+//For running enter DrawCats(number of repeats);
