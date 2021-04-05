ecd134a5bbdbbe5258289d10680abd0c0d6ad912
Vladyslav.Poltoratsk <poltoraxavlad@gmail.com>
2021-04-05 11:19:14 +0300

    Lesson 7

--- jsHomeWorkLesson7.js
+++ jsHomeWorkLesson7.js
@@ -11,17 +11,17 @@ let answerArray = []
 for (let i = 0; i < word.length; i++) {
     answerArray[i] = "_"
 }
+let gueesConter = 3;
+let remainingLetters = word.length;
 
-let remainingLetters = word.length
-
-while (remainingLetters > 0) {
-    alert(answerArray.join(" "))
+while (remainingLetters > 0 && gueesConter > 0) {
     let guess = prompt("Угадайте букву или нажмите Отмена для выхода из игры.").toLowerCase()
-    if (guess === null) {
+    if (guess === null || gueesConter === 0) {
         break
     } else if (guess.length !== 1) {
         alert("Пожалуйста, введите только одну букву.")
-    } else {
+    }else {
+        gueesConter--;
         for (let j = 0; j < word.length; j++) {
             if (word[j] === guess) {
                 answerArray[j] = guess.toLowerCase()
@@ -32,4 +32,9 @@ while (remainingLetters > 0) {
 }
 
 alert(answerArray.join(" "))
-alert("Отлично! Было загадано слово " + word.toUpperCase())
+if (gueesConter > 0) {
+    alert(`Отлично! Было загадано слово ${word}`);
+} else {
+    alert(`Не угадали! Было загадано слово ${word}`);
+}
+
